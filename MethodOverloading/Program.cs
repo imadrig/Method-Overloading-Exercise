2dedc976cc1218a60c25d7969a8debd33a921f1e
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-06-28 12:24:15 -0400

    Initial commit and created 3 overload methods

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -6,5 +6,20 @@
         {
             Console.WriteLine("Hello, World!");
         }
+
+        public static int Add(int a ,  int b) 
+        {
+            return a + b;
+        }
+
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+        public static string Add(int a, int b, bool isTrue)
+        {
+            return (isTrue == true) ? (a + b) + "dollars" : "No Dollars";
+        }
+
     }
 }
