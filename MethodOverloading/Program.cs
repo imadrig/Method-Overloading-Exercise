5f2145c41d414a06172c5ee76c7ae078757a944e
Ingrid Madrigal <39933650+imadrig@users.noreply.github.com>
2023-06-28 13:08:10 -0400

    Made adjustments, still working on the dollar method

--- MethodOverloading/Program.cs
+++ MethodOverloading/Program.cs
@@ -4,7 +4,19 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, World!" + Environment.NewLine);
+
+            int a = 1;
+            int b = 6;
+            int sum = Add(a, b);
+
+            double c = 3.85;
+            double d = 2.1;
+            double decimalSum = Add(c, d);
+
+            string dollarsSum = Add(3, 4, true);
+
+            
         }
 
         public static int Add(int a ,  int b) 
@@ -12,14 +24,17 @@
             return a + b;
         }
 
-        public static double Add(double a, double b)
+        public static double Add(double c, double d)
         {
-            return a + b;
+            return c + d;
         }
-        public static string Add(int a, int b, bool isTrue)
+        public static string Add(int e, int f, bool isCheck)
         {
-            return (isTrue == true) ? (a + b) + "dollars" : "No Dollars";
+            int sum = e + f;
+            return (isCheck == true) ? $"{sum} + dollars" : $"{sum}";
         }
+
+
 
     }
 }
